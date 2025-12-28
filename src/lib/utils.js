919eccc42c0741f3f0ccbc4e48d12e057427293e
V <146417241+sekayeo@users.noreply.github.com>
2025-12-28 20:00:59 +0700

    Replace cn function with createPageUrl function

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,3 +1,5 @@
-export function cn(...classes) {
-  return classes.filter(Boolean).join(" ");
+export function createPageUrl(page) {
+  if (page === "Home") return "/";
+  if (page === "Store") return "/store";
+  return "/";
 }
